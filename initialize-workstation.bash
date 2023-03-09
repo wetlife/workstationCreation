4f0a1e70c642dd498a6330c6056a17e22a4c0075
wetlife <wetlife@gmail.com>
2023-03-08 21:03:52 -0800

    add apt repo and get latest i3

--- initialize-workstation.bash
+++ initialize-workstation.bash
@@ -300,10 +300,35 @@ echo
 ############################
 ############################
 
+#######################################################################
+# add apt repo from devs and install i3 v>=4.22 for i3 gaps in config #
+#######################################################################
+read -p 'add apt repo from devs and install i3 v>=4.22 for i3 gaps in config? (y, q, or /.*/ to skip)\n' -n 1 -r
+echo
+if [[ $REPLY =~ ^[qQ]$ ]]
+then
+    exit 0
+elif [[ $REPLY =~ ^[yY]$ ]]
+then
+	echo 'Attempting to add apt repo and download the latest stable i3.'
+	echo 'Refer to https://i3wm.org/docs/repositories.html on error.'
+	/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
+	sudo apt install ./keyring.deb
+	echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+	sudo apt update
+	sudo apt install i3
+	echo
+else
+	echo "SKIPPED: add apt repo and download the latest stable i3"
+	echo "WARNING: if i3 version is <v4.22 then its config will err."
+	i3 --version
+fi
+echo
+
 ##########################################################################
 # set catppuccin-frappe as the colorscheme for qterminal and other tools #
 ##########################################################################
-read -p 'Setup cattpuccin-frappe as colorsheme for qterminal?(y, q, or /.*/ to skip)' -n 1 -r
+read -p 'Setup cattpuccin-frappe as colorsheme for qterminal? (y, q, or /.*/ to skip)' -n 1 -r
 echo
 if [[ $REPLY =~ ^[qQ]$ ]]
 then
